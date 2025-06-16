17a2de0b29d4e890ebb2b508d4f23e07d9f4a43f
rulos26 <rulos26@gmail.com>
2025-06-16 16:00:35 -0500

    Eliminación de la depuración en LoginController.php, removiendo las salidas de prueba relacionadas con la verificación de contraseñas y la redirección según el rol del usuario. Esto simplifica el código y mejora la seguridad al evitar la exposición de información sensible durante el proceso de inicio de sesión.

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -16,67 +16,34 @@ class LoginController {
             $stmt->execute();
             $user = $stmt->fetch();
 
-            // DEBUG: Mostrar SQL y resultado
-            echo '<pre style="background:#222;color:#fff;padding:10px;">';
-            echo "<b>SQL ejecutado:</b> " . $sql . "\n";
-            echo "<b>Parámetros:</b> "; var_dump(['usuario' => $usuario]);
-            echo "<b>Resultado:</b> "; var_dump($user);
-
             if ($user) {
                 $hash = $user['password'];
                 $isPasswordHash = (strlen($hash) > 32); // bcrypt y otros hash modernos son más largos que 32
                 $passwordOk = false;
                 if ($isPasswordHash) {
                     $passwordOk = password_verify($password, $hash);
-                    echo "<b>Tipo de hash:</b> password_hash (bcrypt, Argon2, etc.)\n";
-                    echo "<b>Resultado password_verify:</b> "; var_dump($passwordOk);
-
-                    // Prueba interna con contraseñas quemadas
-                    $test1 = password_verify('0382646740Ju*', $hash);
-                    $test2 = password_verify('0382646740ju*', $hash);
-                    echo "<b>Test password_verify('0382646740Ju*', hash):</b> "; var_dump($test1);
-                    echo "<b>Test password_verify('0382646740ju*', hash):</b> "; var_dump($test2);
                 } else {
                     $passwordOk = (md5($password) === $hash);
-                    echo "<b>Tipo de hash:</b> md5\n";
-                    echo "<b>Resultado md5:</b> "; var_dump($passwordOk);
-
-                    // Prueba interna con contraseñas quemadas
-                    $test1 = (md5('0382646740Ju*') === $hash);
-                    $test2 = (md5('0382646740ju*') === $hash);
-                    echo "<b>Test md5('0382646740Ju*') === hash:</b> "; var_dump($test1);
-                    echo "<b>Test md5('0382646740ju*') === hash:</b> "; var_dump($test2);
                 }
-                echo "<b>Rol detectado:</b> ".$user['rol']."\n";
                 if ($passwordOk) {
                     if ($user['rol'] == 1) {
-                        echo "<b>Redirigiendo a:</b> resources/views/admin/dashboardAdmin.php\n";
-                        echo '</pre>';
                         $_SESSION['user_id'] = $user['cedula'];
                         $_SESSION['username'] = $user['usuario'];
                         $_SESSION['rol'] = $user['rol'];
                         header('Location: resources/views/admin/dashboardAdmin.php');
                     } elseif ($user['rol'] == 2) {
-                        echo "<b>Redirigiendo a:</b> resources/views/evaluador/dashboardEavaluador.php\n";
-                        echo '</pre>';
                         $_SESSION['user_id'] = $user['cedula'];
                         $_SESSION['username'] = $user['usuario'];
                         $_SESSION['rol'] = $user['rol'];
                         header('Location: resources/views/evaluador/dashboardEavaluador.php');
                     } else {
-                        echo "<b>Rol de usuario no válido:</b> ".$user['rol']."\n";
-                        echo '</pre>';
                         return 'Rol de usuario no válido.';
                     }
                     exit();
                 } else {
-                    echo "<b>Contraseña incorrecta</b>\n";
-                    echo '</pre>';
                     return 'Usuario o contraseña incorrectos.';
                 }
             } else {
-                echo "<b>No se encontró el usuario</b>\n";
-                echo '</pre>';
                 return 'Usuario o contraseña incorrectos.';
             }
         } catch (PDOException $e) {
