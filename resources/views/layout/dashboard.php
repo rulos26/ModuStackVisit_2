54a99922c819510c69aafc2ce7d589b8cdf21dce
rulos26 <rulos26@gmail.com>
2025-06-17 12:07:13 -0500

    Actualización de la ruta del archivo CSS en la vista del dashboard, cambiando de una ruta absoluta a una ruta relativa para mejorar la organización del código y facilitar la gestión de recursos.

--- resources/views/layout/dashboard.php
+++ resources/views/layout/dashboard.php
@@ -11,7 +11,7 @@ $usuario = $_SESSION['username'] ?? 'Invitado';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/public/css/styles.css">
+    <link rel="stylesheet" href="../../../public/css/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 </head>
 <body class="bg-light">
