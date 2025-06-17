c3426a1f7d680dc186da0b300d359c2fc65b0c29
rulos26 <rulos26@gmail.com>
2025-06-17 10:27:56 -0500

    Se añade configuración para la visualización de errores en el controlador de cierre de sesión y en el archivo de cierre de sesión, facilitando la depuración durante el desarrollo. Esto incluye la activación de la visualización de errores y la configuración del nivel de reporte de errores.

--- public/cerrar_sesion.php
+++ public/cerrar_sesion.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once '../../app/Controllers/CerrarSesionController.php';
 $controlador = new CerrarSesionController();
 $controlador->cerrar(); // método que destruye la sesión y redirige
