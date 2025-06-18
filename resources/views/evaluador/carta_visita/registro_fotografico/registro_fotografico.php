d5eee0222f5c0b637ce73987b67a18f8284a3cee
rulos26 <rulos26@gmail.com>
2025-06-18 08:08:41 -0500

    Se añaden configuraciones de error en la vista de registro fotográfico, habilitando la visualización de errores y la depuración. Esto facilitará la identificación de problemas durante el desarrollo y mejorará la trazabilidad de errores en la aplicación.

--- resources/views/evaluador/carta_visita/registro_fotografico/registro_fotografico.php
+++ resources/views/evaluador/carta_visita/registro_fotografico/registro_fotografico.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 /* if (!isset($_SESSION['id_usuario'])) {
     // Redirigir a la página de inicio de sesión si no ha iniciado sesión
     header("Location: ../../../error/error.php");
