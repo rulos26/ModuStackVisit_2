5ec28b97aa80e3d35f4f68822b2e1f8b602b0ae0
rulos26 <rulos26@gmail.com>
2025-06-18 07:45:05 -0500

    Se añade información de depuración en la vista de guardar firma, mostrando datos del POST, SESSION y el método de solicitud. Se implementan mensajes de error específicos para casos de falta de cédula en sesión y errores al guardar la firma, mejorando la trazabilidad y el manejo de errores en el proceso.

--- resources/views/evaluador/carta_visita/firma/guardar_firma.php
+++ resources/views/evaluador/carta_visita/firma/guardar_firma.php
@@ -5,6 +5,14 @@ error_reporting(E_ALL);
 
 session_start();
 
+echo '<pre style="background:#222;color:#0f0;padding:1em;">DEBUG: Entrando a guardar_firma.php\n';
+print_r([
+    'POST' => $_POST,
+    'SESSION' => $_SESSION,
+    'REQUEST_METHOD' => $_SERVER['REQUEST_METHOD']
+]);
+echo '</pre>';
+
 require_once __DIR__ . '/../../../../../app/Controllers/FirmaController.php';
 use App\Controllers\FirmaController;
 
@@ -12,16 +20,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['firma_digital'])) {
     $firmaBase64 = $_POST['firma_digital'];
     $id_cedula = $_SESSION['cedula_autorizacion'] ?? $_SESSION['user_id'] ?? null;
     if (!$id_cedula) {
+        echo '<pre style="background:#222;color:#f00;padding:1em;">DEBUG: No hay cédula en sesión</pre>';
         header('Location: /ModuStackVisit_2/resources/views/error/error.php?from=guardar_firma&msg=No hay cédula en sesión');
         exit();
     }
     $resultado = FirmaController::guardarFirma($firmaBase64, $id_cedula);
     if ($resultado !== true) {
+        echo '<pre style="background:#222;color:#f00;padding:1em;">DEBUG: Error al guardar firma: ' . htmlspecialchars($resultado) . '</pre>';
         $_SESSION['error'] = $resultado;
         header('Location: /ModuStackVisit_2/resources/views/error/error.php?from=guardar_firma&msg=' . urlencode($resultado));
         exit();
     }
 } else {
+    echo '<pre style="background:#222;color:#f00;padding:1em;">DEBUG: Acceso no permitido o falta firma_digital</pre>';
     header('Location: /ModuStackVisit_2/resources/views/error/error.php?from=guardar_firma&msg=Acceso no permitido');
     exit();
 } 
