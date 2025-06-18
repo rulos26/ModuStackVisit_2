ff048cd5ed084def9cea120101477f95dcc0e8ef
rulos26 <rulos26@gmail.com>
2025-06-18 10:55:58 -0500

    Se refactoriza el controlador DemoPdfController para utilizar la clase Database en lugar de la conexión global a MySQL. Se actualiza la consulta SQL para usar parámetros nombrados, mejorando la seguridad y legibilidad del código.

--- app/Controllers/DemoPdfController.php
+++ app/Controllers/DemoPdfController.php
@@ -2,25 +2,24 @@
 
 namespace App\Controllers;
 
+require_once __DIR__ . '/../Database/Database.php';
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../conn/conexion.php';
 
 use Dompdf\Dompdf;
+use App\Database\Database;
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ob_start();
 class DemoPdfController {
     public static function generarEjemplo() {
-        global $mysqli;
+        $db = Database::getInstance()->getConnection();
         $cedula = '1231211322';
-        $sql = "SELECT `id`, `cedula`, `nombres`, `direccion`, `localidad`, `barrio`, `telefono`, `celular`, `fecha`, `autorizacion`, `correo` FROM `autorizaciones` WHERE cedula=? LIMIT 1";
-        $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param('s', $cedula);
+        $sql = "SELECT `id`, `cedula`, `nombres`, `direccion`, `localidad`, `barrio`, `telefono`, `celular`, `fecha`, `autorizacion`, `correo` FROM `autorizaciones` WHERE cedula=:cedula LIMIT 1";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':cedula', $cedula);
         $stmt->execute();
-        $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
-        $stmt->close();
+        $row = $stmt->fetch(\PDO::FETCH_ASSOC);
 
         // Crear instancia de Dompdf
         $dompdf = new Dompdf();
