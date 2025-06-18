c3e020caff9cdef8e8637c3d4723a27eeea9dcac
rulos26 <rulos26@gmail.com>
2025-06-18 14:06:44 -0500

    Se actualiza la plantilla PDF para utilizar la variable $row1 en lugar de $row, asegurando que se muestren correctamente los datos de la persona identificada en la carta de autorización. Se mejora la consistencia en la presentación de la información personal en el documento generado.

--- resources/views/pdf/plantilla_pdf.php
+++ resources/views/pdf/plantilla_pdf.php
@@ -16,30 +16,30 @@
     <tr>
         <td colspan="12" style="border: 1px solid black; text-align: justify;">
             <div class="carta">
-                <p><strong>#Yo <?= htmlspecialchars($row['nombres'] ?? '') ?></strong></p>
-                <p>Identificado (a) con cédula de ciudadanía No. <strong><?= htmlspecialchars($row['cedula'] ?? '') ?></strong></p>
+                <p><strong>#Yo <?= htmlspecialchars($row1['nombres'] ?? '') ?></strong></p>
+                <p>Identificado (a) con cédula de ciudadanía No. <strong><?= htmlspecialchars($row1['cedula'] ?? '') ?></strong></p>
                 <p>Expedida en: <strong>Bogotá D.C</strong></p>
-                <p><?= nl2br(htmlspecialchars($row['autorizacion'] ?? '')) ?></p>
+                <p><?= nl2br(htmlspecialchars($row1['autorizacion'] ?? '')) ?></p>
             </div>
         </td>
     </tr>
     <tr>
         <td colspan="3" style="font-weight: bold; background-color: #ABABAB; border: 1px solid black;">Dirección</td>
-        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row['direccion'] ?? '') ?></td>
+        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row1['direccion'] ?? '') ?></td>
         <td colspan="3" style="font-weight: bold; background-color: #ABABAB; border: 1px solid black;">Barrio</td>
-        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row['barrio'] ?? '') ?></td>
+        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row1['barrio'] ?? '') ?></td>
     </tr>
     <tr>
         <td colspan="3" style="font-weight: bold; background-color: #ABABAB; border: 1px solid black;">Localidad</td>
-        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row['localidad'] ?? '') ?></td>
+        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row1['localidad'] ?? '') ?></td>
         <td colspan="3" style="font-weight: bold; background-color: #ABABAB; border: 1px solid black;">Teléfono Fijo</td>
-        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row['telefono'] ?? '') ?></td>
+        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row1['telefono'] ?? '') ?></td>
     </tr>
     <tr>
         <td colspan="3" style="font-weight: bold; background-color: #ABABAB; border: 1px solid black;">Celular</td>
-        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row['celular'] ?? '') ?></td>
+        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row1['celular'] ?? '') ?></td>
         <td colspan="3" style="font-weight: bold; background-color: #ABABAB; border: 1px solid black;">Correo electrónico</td>
-        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row['correo'] ?? '') ?></td>
+        <td colspan="3" style="border: 1px solid black;"><?= htmlspecialchars($row1['correo'] ?? '') ?></td>
     </tr>
 </tbody>
 </table>
