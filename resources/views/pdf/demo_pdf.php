c219a5d209b22883581f3cbe6d506aa08ff6961b
rulos26 <rulos26@gmail.com>
2025-06-18 10:47:16 -0500

    Actualiza la ruta de inclusión del controlador DemoPdfController en la vista demo_pdf.php, mejorando la estructura del proyecto y asegurando el correcto funcionamiento de la generación de PDFs.

--- resources/views/pdf/demo_pdf.php
+++ resources/views/pdf/demo_pdf.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ob_start();
-require_once __DIR__ . '/../../../../app/Controllers/DemoPdfController.php';
+require_once __DIR__ . '/../../../app/Controllers/DemoPdfController.php';
 use App\Controllers\DemoPdfController;
 
 DemoPdfController::generarEjemplo(); 
