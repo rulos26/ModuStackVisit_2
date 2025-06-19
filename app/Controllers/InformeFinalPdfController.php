a717841e48938b4762216cd556dc9c8316ddee65
rulos26 <rulos26@gmail.com>
2025-06-19 11:19:04 -0500

    Actualiza las uniones en 'InformeFinalPdfController.php' para utilizar las tablas correctas y mejorar la consistencia en las consultas SQL. Se cambian los nombres de las tablas y se ajustan las uniones para reflejar la estructura actual de la base de datos.

--- app/Controllers/InformeFinalPdfController.php
+++ app/Controllers/InformeFinalPdfController.php
@@ -138,11 +138,11 @@ class InformeFinalPdfController {
                     m.nombre as lugar_nacimiento_municipio
                 FROM evaluados e
                 LEFT JOIN opc_tipo_documentos td ON e.tipo_documento_id = td.id
-                LEFT JOIN opc_ciudades c ON e.ciudad_id = c.id
+                LEFT JOIN opciones c ON e.ciudad_id = c.id
                 LEFT JOIN opc_rh rh ON e.rh_id = rh.id
-                LEFT JOIN opc_estatura est ON e.estatura_id = est.id
-                LEFT JOIN opc_estado_civil ec ON e.estado_civil_id = ec.id
-                LEFT JOIN opc_municipios m ON e.lugar_nacimiento_municipio_id = m.id
+                LEFT JOIN opc_estaturas est ON e.estatura_id = est.id
+                LEFT JOIN opc_estado_civiles ec ON e.estado_civil_id = ec.id
+                LEFT JOIN municipios m ON e.lugar_nacimiento_municipio_id = m.id
                 WHERE e.id_cedula = ?
             ";
             
@@ -234,7 +234,7 @@ class InformeFinalPdfController {
                     cc.*,
                     tc.nombre as tiene_camara
                 FROM camara_comercio cc
-                LEFT JOIN opc_tipo_respuesta tc ON cc.tiene_camara_id = tc.id
+                LEFT JOIN opc_estados tc ON cc.tiene_camara_id = tc.id
                 WHERE cc.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
             ";
             $stmt = $this->mysqli->prepare($query);
@@ -265,13 +265,13 @@ class InformeFinalPdfController {
                     es_medicamento.nombre as nombre_tipo_medicamento,
                     es_alcohol.nombre as nombre_ingiere_alcohol,
                     es_fuma.nombre as nombre_fuma
-                FROM estado_salud es
-                LEFT JOIN opc_tipo_respuesta es_estado ON es.estado_salud_id = es_estado.id
-                LEFT JOIN opc_tipo_respuesta es_enfermedad ON es.tipo_enfermedad_id = es_enfermedad.id
-                LEFT JOIN opc_tipo_respuesta es_limitacion ON es.limitacion_fisica_id = es_limitacion.id
-                LEFT JOIN opc_tipo_respuesta es_medicamento ON es.tipo_medicamento_id = es_medicamento.id
-                LEFT JOIN opc_tipo_respuesta es_alcohol ON es.ingiere_alcohol_id = es_alcohol.id
-                LEFT JOIN opc_tipo_respuesta es_fuma ON es.fuma_id = es_fuma.id
+                FROM estados_salud es
+                LEFT JOIN opc_estados es_estado ON es.estado_salud_id = es_estado.id
+                LEFT JOIN opc_estados es_enfermedad ON es.tipo_enfermedad_id = es_enfermedad.id
+                LEFT JOIN opc_estados es_limitacion ON es.limitacion_fisica_id = es_limitacion.id
+                LEFT JOIN opc_estados es_medicamento ON es.tipo_medicamento_id = es_medicamento.id
+                LEFT JOIN opc_estados es_alcohol ON es.ingiere_alcohol_id = es_alcohol.id
+                LEFT JOIN opc_estados es_fuma ON es.fuma_id = es_fuma.id
                 WHERE es.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
             ";
             $stmt = $this->mysqli->prepare($query);
@@ -300,7 +300,7 @@ class InformeFinalPdfController {
                     e.nombre as estado_civil_nombre
                 FROM composicion_familiar cf
                 LEFT JOIN opc_parentesco p ON cf.parentesco_id = p.id
-                LEFT JOIN opc_estado_civil e ON cf.estado_civil_id = e.id
+                LEFT JOIN opc_estado_civiles e ON cf.estado_civil_id = e.id
                 WHERE cf.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY cf.id
             ";
@@ -336,8 +336,8 @@ class InformeFinalPdfController {
                     e.nombre as estado_civil_nombre
                 FROM informacion_pareja ip
                 LEFT JOIN opc_tipo_documentos td ON ip.tipo_documento_id = td.id
-                LEFT JOIN opc_ciudades c ON ip.ciudad_id = c.id
-                LEFT JOIN opc_estado_civil e ON ip.estado_civil_id = e.id
+                LEFT JOIN opciones c ON ip.ciudad_id = c.id
+                LEFT JOIN opc_estado_civiles e ON ip.estado_civil_id = e.id
                 WHERE ip.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
             ";
             $stmt = $this->mysqli->prepare($query);
@@ -393,9 +393,9 @@ class InformeFinalPdfController {
                     i.*,
                     c.nombre as categoria_nombre,
                     e.nombre as estado_nombre
-                FROM inventario i
-                LEFT JOIN opc_categoria_inventario c ON i.categoria_id = c.id
-                LEFT JOIN opc_estado_inventario e ON i.estado_id = e.id
+                FROM inventario_enseres i
+                LEFT JOIN opc_inventario_enseres c ON i.categoria_id = c.id
+                LEFT JOIN opc_estados e ON i.estado_id = e.id
                 WHERE i.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY i.id
             ";
@@ -428,9 +428,9 @@ class InformeFinalPdfController {
                     s.*,
                     ts.nombre as tipo_servicio_nombre,
                     e.nombre as estado_nombre
-                FROM servicios s
-                LEFT JOIN opc_tipo_servicio ts ON s.tipo_servicio_id = ts.id
-                LEFT JOIN opc_estado_servicio e ON s.estado_id = e.id
+                FROM servicios_publicos s
+                LEFT JOIN opc_servicios_publicos ts ON s.tipo_servicio_id = ts.id
+                LEFT JOIN opc_estados e ON s.estado_id = e.id
                 WHERE s.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY s.id
             ";
@@ -464,8 +464,8 @@ class InformeFinalPdfController {
                     tp.nombre as tipo_patrimonio_nombre,
                     e.nombre as estado_nombre
                 FROM patrimonio p
-                LEFT JOIN opc_tipo_patrimonio tp ON p.tipo_patrimonio_id = tp.id
-                LEFT JOIN opc_estado_patrimonio e ON p.estado_id = e.id
+                LEFT JOIN opc_tipo_inversion tp ON p.tipo_patrimonio_id = tp.id
+                LEFT JOIN opc_estados e ON p.estado_id = e.id
                 WHERE p.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY p.id
             ";
@@ -499,7 +499,7 @@ class InformeFinalPdfController {
                     b.nombre as banco_nombre,
                     tc.nombre as tipo_cuenta_nombre
                 FROM cuentas_bancarias cb
-                LEFT JOIN opc_bancos b ON cb.banco_id = b.id
+                LEFT JOIN opc_entidad b ON cb.banco_id = b.id
                 LEFT JOIN opc_tipo_cuenta tc ON cb.tipo_cuenta_id = tc.id
                 WHERE cb.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY cb.id
@@ -534,8 +534,8 @@ class InformeFinalPdfController {
                     tp.nombre as tipo_pasivo_nombre,
                     e.nombre as estado_nombre
                 FROM pasivos p
-                LEFT JOIN opc_tipo_pasivo tp ON p.tipo_pasivo_id = tp.id
-                LEFT JOIN opc_estado_pasivo e ON p.estado_id = e.id
+                LEFT JOIN opc_entidad tp ON p.tipo_pasivo_id = tp.id
+                LEFT JOIN opc_estados e ON p.estado_id = e.id
                 WHERE p.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY p.id
             ";
@@ -568,8 +568,8 @@ class InformeFinalPdfController {
                     a.*,
                     ta.nombre as tipo_aportante_nombre,
                     p.nombre as parentesco_nombre
-                FROM aportantes a
-                LEFT JOIN opc_tipo_aportante ta ON a.tipo_aportante_id = ta.id
+                FROM aportante a
+                LEFT JOIN opc_ocupacion ta ON a.tipo_aportante_id = ta.id
                 LEFT JOIN opc_parentesco p ON a.parentesco_id = p.id
                 WHERE a.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY a.id
@@ -602,8 +602,8 @@ class InformeFinalPdfController {
                 SELECT 
                     i.*,
                     ti.nombre as tipo_ingreso_nombre
-                FROM ingresos i
-                LEFT JOIN opc_tipo_ingreso ti ON i.tipo_ingreso_id = ti.id
+                FROM ingresos_mensuales i
+                LEFT JOIN opc_ocupacion ti ON i.tipo_ingreso_id = ti.id
                 WHERE i.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY i.id
             ";
@@ -635,8 +635,8 @@ class InformeFinalPdfController {
                 SELECT 
                     g.*,
                     tg.nombre as tipo_gasto_nombre
-                FROM gastos g
-                LEFT JOIN opc_tipo_gasto tg ON g.tipo_gasto_id = tg.id
+                FROM gasto g
+                LEFT JOIN opc_ocupacion tg ON g.tipo_gasto_id = tg.id
                 WHERE g.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY g.id
             ";
@@ -670,8 +670,8 @@ class InformeFinalPdfController {
                     n.nombre as nivel_educativo_nombre,
                     e_estado.nombre as estado_nombre
                 FROM estudios e
-                LEFT JOIN opc_nivel_educativo n ON e.nivel_educativo_id = n.id
-                LEFT JOIN opc_estado_estudio e_estado ON e.estado_id = e_estado.id
+                LEFT JOIN opc_nivel_academico n ON e.nivel_educativo_id = n.id
+                LEFT JOIN opc_estados e_estado ON e.estado_id = e_estado.id
                 WHERE e.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY e.id
             ";
@@ -705,8 +705,8 @@ class InformeFinalPdfController {
                     c.nombre as cargo_nombre,
                     e.nombre as estado_nombre
                 FROM experiencia_laboral el
-                LEFT JOIN opc_cargos c ON el.cargo_id = c.id
-                LEFT JOIN opc_estado_experiencia e ON el.estado_id = e.id
+                LEFT JOIN opc_ocupacion c ON el.cargo_id = c.id
+                LEFT JOIN opc_estados e ON el.estado_id = e.id
                 WHERE el.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY el.id
             ";
@@ -740,8 +740,8 @@ class InformeFinalPdfController {
                     tj.nombre as tipo_judicial_nombre,
                     e.nombre as estado_nombre
                 FROM informacion_judicial ij
-                LEFT JOIN opc_tipo_judicial tj ON ij.tipo_judicial_id = tj.id
-                LEFT JOIN opc_estado_judicial e ON ij.estado_id = e.id
+                LEFT JOIN opc_informacion_judicial tj ON ij.tipo_judicial_id = tj.id
+                LEFT JOIN opc_estados e ON ij.estado_id = e.id
                 WHERE ij.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY ij.id
             ";
@@ -773,8 +773,8 @@ class InformeFinalPdfController {
                 SELECT 
                     cf.*,
                     c.nombre as concepto_nombre
-                FROM concepto_final cf
-                LEFT JOIN opc_conceptos c ON cf.concepto_id = c.id
+                FROM concepto_final_evaluador cf
+                LEFT JOIN opc_concepto_final c ON cf.concepto_id = c.id
                 WHERE cf.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
             ";
             $stmt = $this->mysqli->prepare($query);
@@ -802,8 +802,8 @@ class InformeFinalPdfController {
                     d.nombre as departamento_nombre,
                     m.nombre as municipio_nombre
                 FROM ubicacion u
-                LEFT JOIN opc_departamentos d ON u.departamento_id = d.id
-                LEFT JOIN opc_municipios m ON u.municipio_id = m.id
+                LEFT JOIN departamento d ON u.departamento_id = d.id
+                LEFT JOIN municipios m ON u.municipio_id = m.id
                 WHERE u.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
             ";
             $stmt = $this->mysqli->prepare($query);
@@ -829,8 +829,8 @@ class InformeFinalPdfController {
                 SELECT 
                     ef.*,
                     te.nombre as tipo_evidencia_nombre
-                FROM evidencias_fotograficas ef
-                LEFT JOIN opc_tipo_evidencia te ON ef.tipo_evidencia_id = te.id
+                FROM evidencia_fotografica ef
+                LEFT JOIN opc_estados te ON ef.tipo_evidencia_id = te.id
                 WHERE ef.evaluado_id = (SELECT id FROM evaluados WHERE id_cedula = ?)
                 ORDER BY ef.id
             ";
