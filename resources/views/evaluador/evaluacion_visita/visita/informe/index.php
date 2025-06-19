ac60194de9a2eefada0a16b2d5ae76f65ab2f945
rulos26 <rulos26@gmail.com>
2025-06-19 02:09:27 -0500

    Agrega configuración para mostrar errores en desarrollo en 'index.php', facilitando la depuración durante el desarrollo del informe de visita.

--- resources/views/evaluador/evaluacion_visita/visita/informe/index.php
+++ resources/views/evaluador/evaluacion_visita/visita/informe/index.php
@@ -1,4 +1,8 @@
 <?php
+// Mostrar errores solo en desarrollo
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 require_once('../../../../../librery/tcpdf.php');
 include '../../../../../conn/conexion.php';
 include 'sql/evaluados.php';
