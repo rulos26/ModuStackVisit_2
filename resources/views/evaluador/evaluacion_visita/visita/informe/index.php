3b9d2d95fa4e2c8541cf52b10f983e9b426ef8c3
rulos26 <rulos26@gmail.com>
2025-06-19 02:12:57 -0500

    Corrige la ruta de inclusión del archivo 'tcpdf.php' en 'index.php' para asegurar que se cargue correctamente, mejorando la funcionalidad del informe de visita.

--- resources/views/evaluador/evaluacion_visita/visita/informe/index.php
+++ resources/views/evaluador/evaluacion_visita/visita/informe/index.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-require_once('../../../../../librery/tcpdf.php');
+require_once(__DIR__ . '/../../../../../../librery/tcpdf.php');
 include '../../../../../conn/conexion.php';
 include 'sql/evaluados.php';
 include 'sql/perfil.php';
