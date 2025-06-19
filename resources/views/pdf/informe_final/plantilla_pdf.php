6f5ce754c552a3ff26780238ad67c660045e5e63
rulos26 <rulos26@gmail.com>
2025-06-19 12:15:44 -0500

    Agrega un logo opcional en la plantilla del informe PDF, mejorando la presentación visual del documento. Se incluye un mensaje alternativo en caso de que el logo no esté disponible.

--- resources/views/pdf/informe_final/plantilla_pdf.php
+++ resources/views/pdf/informe_final/plantilla_pdf.php
@@ -1,3 +1,14 @@
 <div style="border: 2px solid rgb(175, 0, 0); padding: 12px;">
-<?= htmlspecialchars($cedula) ?>
+    <table cellpadding="5" style="width: 100%; margin-bottom: 10px;">
+        <tr>
+            <td width="100%" style="border: 1px solid rgb(175,0,0); text-align: center;">
+                <?php if (!empty($logo_b64)): ?>
+                    <img src="<?= $logo_b64 ?>" alt="Logo" style=" width: 100%; height:103px;">
+                <?php else: ?>
+                    <span style="color: #888;">Logo no disponible</span>
+                <?php endif; ?>
+            </td>
+        </tr>
+    </table>
+    <?= htmlspecialchars($cedula) ?>
 </div>
