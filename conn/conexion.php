8113784fcf940e54457ef7a02405e59d5b761476
rulos26 <rulos26@gmail.com>
2025-06-19 02:04:19 -0500

    Actualiza la redirección en 'registro_fotos.php' para dirigir a 'informe/index.php' tras registrar las fotos, mejorando la experiencia de navegación del usuario.

---
+++ conn/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$dbname = "u130454517_modulo_vista"; // Cambia nombre_base_de_datos por el nombre de tu base de datos
+$username = 'u130454517_root';
+$password = '0382646740Ju*';
+$charset = 'utf8mb4';
+
+//dd
+// Crear conexión
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($mysqli->connect_error) {
+    die("Error de conexión: " . $mysqli->connect_error);
+}
+?>
