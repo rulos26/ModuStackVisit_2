f102b845fed8165565d4a0305569fd0890535abd
rulos26 <rulos26@gmail.com>
2025-06-19 20:29:21 -0500

    Agrega configuración para el manejo de errores en el controlador de informes finales PDF, habilitando la visualización de errores y estableciendo el nivel de reporte a E_ALL.

--- app/Controllers/InformeFinalPdfController.php
+++ app/Controllers/InformeFinalPdfController.php
@@ -8,6 +8,10 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use Dompdf\Dompdf;
 use App\Database\Database;
 use Exception;
+//pon el manejode errores
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 class InformeFinalPdfController {
     
