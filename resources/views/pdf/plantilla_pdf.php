21e749cd84ae400f7d9a2b247518e77db134e2ce
rulos26 <rulos26@gmail.com>
2025-07-02 16:00:42 -0500

    Modifica el padding del contenedor en la plantilla PDF, reduciendo el espacio de 12px a 1px. Este ajuste mejora la presentación visual del documento al optimizar el uso del espacio.

--- resources/views/pdf/plantilla_pdf.php
+++ resources/views/pdf/plantilla_pdf.php
@@ -32,7 +32,7 @@
         text-align: right;
     }
 </style>
-<div style="border: 2px solid rgb(0, 175, 67); padding: 12px;">
+<div style="border: 2px solid rgb(0, 175, 67); padding: 1px;">
     <table cellpadding="5" style="width: 100%; margin-bottom: 10px;">
         <tr>
             <td width="100%" style="border: 1px solid rgb(175,0,0); text-align: center;">
