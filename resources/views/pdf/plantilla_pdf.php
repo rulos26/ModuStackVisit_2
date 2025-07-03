df460906b01c4c2b671153d3128106a4de1f6df3
rulos26 <rulos26@gmail.com>
2025-07-03 07:55:42 -0500

    Se añade un párrafo sobre la legislación adicional requerida en la plantilla PDF, incluyendo autorizaciones para consultas ante la Fiscalía y centrales de riesgo, cumpliendo con las leyes pertinentes. Además, se optimiza la visualización del correo electrónico para mostrar 'N/A' si el campo está vacío, mejorando la presentación de la información.

--- resources/views/pdf/plantilla_pdf.php
+++ resources/views/pdf/plantilla_pdf.php
@@ -75,6 +75,22 @@
                             suficientes, a fin de establecer la veracidad de la información suministrada, para que
                             en el momento que se haga necesaria se utilice como prueba. Contemplando en el
                             DECRETO 1266 DE 2008. </p>
+                        <p> Legislación adicional solicitada (SPOA - SIJUF y centrales de riesgo):
+                            La observación hace referencia a la necesidad de incorporar la autorización de consulta ante la Fiscalía
+                            General de la Nación (SPOA - SIJUF) y la autorización para consulta en centrales de riesgo, tal como lo
+                            exigen las leyes 1266 de 2008, 1581 de 2012 y 172 de 2014.
+                            Adjuntamos en este correo el texto sugerido que puede incorporarse tal cual en el formato PDF, de
+                            manera que se cumpla con los requerimientos legales.
+                            “autorizo la visita domiciliaria y manifiesto de manera libre, expresa e informada que autorizo
+                            plenamente a la empresa GRUPO DE TAREAS EMPRESARIALES, con NIT 830.142.258-3, para realizar
+                            la verificación de antecedentes judiciales, penales, disciplinarios y fiscales ante las autoridades
+                            competentes, incluyendo la Fiscalía General de la Nación mediante consulta en el sistema SPOA – SIJUF,
+                            así como la verificación académica, laboral, domiciliaria y la consulta en centrales de información
+                            financiera y crediticia (Data crédito, CIFIN u otras), de conformidad con lo establecido en el artículo 13
+                            ley 1581 del 2012, la Ley 172 de 2014, Ley 1266 de 2008, Ley 1581 de 2012 y el Decreto 1266 de 2008,
+                            Para tomar las pruebas necesarias y suficientes, a fin de establecer la veracidad de la información
+                            suministrada, para que en el momento que se haga necesaria se utilice como prueba. Contemplado en el
+                            DECRETO 1266 de 2008.</p>
                     </div>
                 </td>
             </tr>
@@ -96,17 +112,17 @@
                 <td colspan="3" style="font-weight: bold; background-color: #ABABAB; border: 1px solid black;">Correo electrónico</td>
                 <td colspan="3" style="border: 1px solid black;">
                     <?php
-                        $correo = $row1['correo'] ?? '';
-                        if (!empty($correo)) {
-                            $atPos = strpos($correo, '@');
-                            if ($atPos !== false && $atPos >= 16) {
-                                echo htmlspecialchars(substr($correo, 0, $atPos)) . '<br>@' . htmlspecialchars(substr($correo, $atPos + 1));
-                            } else {
-                                echo htmlspecialchars($correo);
-                            }
+                    $correo = $row1['correo'] ?? '';
+                    if (!empty($correo)) {
+                        $atPos = strpos($correo, '@');
+                        if ($atPos !== false && $atPos >= 16) {
+                            echo htmlspecialchars(substr($correo, 0, $atPos)) . '<br>@' . htmlspecialchars(substr($correo, $atPos + 1));
                         } else {
-                            echo 'N/A';
+                            echo htmlspecialchars($correo);
                         }
+                    } else {
+                        echo 'N/A';
+                    }
                     ?>
                 </td>
             </tr>
