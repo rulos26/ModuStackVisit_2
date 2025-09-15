d8440e2cb42144408c4675b45bc4d8a5c2bdde10
juan.diaz <rulos26@gmail.com>
2025-09-14 20:05:38 -0500

    Se añadieron configuraciones para mostrar errores en 'session.php', activando la visualización de errores y advertencias para facilitar la depuración durante el desarrollo.

--- resources/views/evaluador/evaluacion_visita/visita/session.php
+++ resources/views/evaluador/evaluacion_visita/visita/session.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Iniciar la sesión si no está iniciada
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
